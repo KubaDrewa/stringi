e6ca974529c35c82785b450e78b2a1055e9c5aed
Kuba <kubadrewa9@gmail.com>
2024-01-17 14:13:03 +0100

    Zaczynamy omawianie zmiennych typu string

---
+++ zmiennetypustring.py
@@ -0,0 +1,12 @@
+#Zmienne typu string
+
+# 1. Zmienne typu string są niemutowalne - nie można ich modyfikować
+napis1 = 'informatyka'
+''' napis1[2] = 's'
+print(napis1) '''
+
+#2. Indeksowanie znaków
+print(napis1[2])
+print(napis1[2:5])
+
+# 3.
